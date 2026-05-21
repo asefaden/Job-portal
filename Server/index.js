37a99879119761c95016bd3f650dbc3e1ecd7af9
Assefa Demeke <assede1122@gmail.com>
2026-05-21 12:00:51 +0300

    first commit

---
+++ Server/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+dotenv.config({});
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./utils/db.js";
+import userRouter from "./routes/user.routes.js";
+import companyRouter from "./routes/company.routes.js";
+import jobRouter from "./routes/job.routes.js";
+import applicantionRouter from "./routes/application.routes.js";
+
+const app = express();
+
+connectDB()
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || ' https://job-portal.app.aletcloud.com',
+    credentials: true,
+}
+app.use(cors(corsOptions));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server");
+})
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/company", companyRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicantionRouter);
+
+
+app.listen(PORT, () => {
+    
+    console.log(`Server running at port ${PORT}`);
+})
