c2d8ac90d23dda6d4299040e0d5feaf562248e09
Assefa Demeke <assede1122@gmail.com>
2026-05-21 12:16:56 +0300

    first commit change

--- Server/index.js
+++ Server/index.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || ' https://job-portal.app.aletcloud.com',
+    origin: process.env.FRONTEND_URL || 'https://job-portal.app.aletcloud.com',
     credentials: true,
 }
 app.use(cors(corsOptions));
 
-app.get("/",(req,res)=>{
+app.get("/Api",(req,res)=>{
     res.send("Welcome to the server");
 })
 
