1724750a9f0557709735ad3bec04e6c6cf9642b4
Assefa Demeke <assede1122@gmail.com>
2026-05-21 17:06:32 +0300

    Fix /Api route and CORS issue

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'https://job1.app.aletcloud.com',
+    origin: process.env.FRONTEND_URL || ' https://job-portal-1.app.aletcloud.com',
     credentials: true,
 }
 app.use(cors(corsOptions));
