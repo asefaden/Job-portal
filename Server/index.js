e4cacf0fb292f1d021e2b583e57a0a689c1c213a
Assefa Demeke <assede1122@gmail.com>
2026-05-23 11:32:29 +0300

    second commit

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || ' https://job-port.app.aletcloud.com',
+    origin: process.env.FRONTEND_URL || 'https://job-portal1.app.aletcloud.com',
     credentials: true,
 }
 app.use(cors(corsOptions));
