e0baefd378e55e2cec4b6b66d325a2d39ebf4c5e
Assefa Demeke <assede1122@gmail.com>
2026-05-26 09:59:24 +0300

    your commit message

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({});
+
+dotenv.config();
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('MongoDB Connected...')
-    } catch (error) {
-        console.error(error.message)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // valid here
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
 
-
-export default connectDB
+export default connectDB;
